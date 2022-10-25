0b2caeba8bc4561d1c705ee80afc950b356772e7
= <=>
2022-10-25 08:58:03 -0700

    Completed 2015 Day 6

--- 2015/day06/parse.js
+++ 2015/day06/parse.js
@@ -4,7 +4,19 @@ const fs = require("fs");
 const input = fs
   .readFileSync(path.join(__dirname, "input.txt"), "utf8")
   .toString()
-  .trim();
+  .trim()
+  .split("\n")
+  .map((instruction) => {
+    let split = instruction.split(" ");
+    let action = split[0] === "turn" ? split[1] : split[0];
+    let start = split[split.length - 3].split(",");
+    let end = split[split.length - 1].split(",");
+    return {
+      action,
+      start: { x: parseInt(start[0]), y: parseInt(start[1]) },
+      end: { x: parseInt(end[0]), y: parseInt(end[1]) },
+    };
+  });
 
 module.exports = {
   input,
--- 2015/day06/part1.js
+++ 2015/day06/part1.js
@@ -1,1 +1,23 @@
 const { input } = require("./parse");
+
+let lightBoard = new Array(1000).fill(0).map(() => new Array(1000).fill(0));
+
+for (let instruction of input) {
+  for (let x = instruction.start.x; x <= instruction.end.x; x++) {
+    for (let y = instruction.start.y; y <= instruction.end.y; y++) {
+      if (instruction.action === "on") lightBoard[x][y] = 1;
+      else if (instruction.action === "off") lightBoard[x][y] = 0;
+      else if (instruction.action === "toggle")
+        lightBoard[x][y] = lightBoard[x][y] === 0 ? 1 : 0;
+    }
+  }
+}
+
+let count = 0;
+for (let i = 0; i < lightBoard.length; i++) {
+  for (let j = 0; j < lightBoard[i].length; j++) {
+    if (lightBoard[i][j] === 1) count++;
+  }
+}
+
+console.log(count);

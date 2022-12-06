e19cda8b6d94690968cdb64e764edad1ce929c71
= <=>
2022-12-05 21:22:23 -0800

    Completed 2022 Day 6

---
+++ 2022/day06/part1.js
@@ -0,0 +1,20 @@
+const { input } = require("./parse");
+
+const DISTINCT_CHARS = 4;
+
+const allUnique = (arr) => {
+  let visited = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (visited.has(arr[i])) return false;
+
+    visited.add(arr[i]);
+  }
+  return true;
+};
+
+for (let i = 0; i < input.length - 4; i++) {
+  if (allUnique(input.slice(i, i + DISTINCT_CHARS))) {
+    console.log(i + DISTINCT_CHARS);
+    break;
+  }
+}

02e4638d21d6e1f00f34ea4c26cc42a9a5f1ed23
= <=>
2022-12-20 01:11:00 -0800

    Completed 2022 Day 20

---
+++ 2022/day20/parse.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fs = require("fs");
+
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item, idx) => {
+    return { num: parseInt(item), idx };
+  });
+
+module.exports = {
+  input,
+};
---
+++ 2022/day20/part1.js
@@ -0,0 +1,48 @@
+const { input } = require("./parse");
+console.time("ExecutionTime");
+
+const insert = (arr, index, item) => {
+  arr.splice(index, 0, item);
+};
+
+const remove = (arr, index) => {
+  let idx = mixed.indexOf(mixed.find((item) => item.idx === index));
+  arr.splice(idx, 1);
+};
+
+let mixed = [...input];
+for (let i = 0; i < input.length; i++) {
+  let num = input[i].num;
+  let idx = mixed.indexOf(input[i]);
+  let newIdx = idx;
+  if (num > 0) num %= mixed.length - 1;
+  if (num < 0) num = ((num * -1) % (mixed.length - 1)) * -1;
+  while (num > 0) {
+    if (newIdx == mixed.length - 1) newIdx = 0;
+
+    newIdx++;
+    num--;
+    if (newIdx == mixed.length - 1) newIdx = 0;
+  }
+  while (num < 0) {
+    if (newIdx == 0) newIdx = mixed.length - 1;
+    newIdx--;
+    num++;
+    if (newIdx == 0) newIdx = mixed.length - 1;
+  }
+  remove(mixed, input[i].idx);
+
+  insert(mixed, newIdx, input[i]);
+}
+
+let idxOf0 = mixed.indexOf(mixed.find((item) => item.num === 0));
+
+let sum = 0;
+for (let i = 1000; i <= 3000; i += 1000) {
+  let groveCoordinate = (i + idxOf0) % mixed.length;
+  sum += mixed[groveCoordinate].num;
+}
+
+console.log(sum);
+
+console.timeEnd("ExecutionTime");
